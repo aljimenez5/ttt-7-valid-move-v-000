16379f66d63f5c2f253560acc5cbdfb5b39464da
arvida.jimenez@gmail.com <arvida.jimenez@gmail.com>
2018-07-23 06:42:15 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,8 +3,8 @@ def valid_move?(board, index)
     true
   elsif index.between?(0, 8) && board.include?('X' || 'O')
     true
-  elsif position_taken?
-    false
+  elsif !position_taken?
+    true
   else
     false
   end
